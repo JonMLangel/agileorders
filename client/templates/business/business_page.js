8eedaff752830e4aa50fefbe862e206e43965708
John Popilek <john.popilek@gmail.com>
2017-05-21 13:36:15 -0500

    added messages for not authorized scenario

--- client/templates/business/business_page.js
+++ client/templates/business/business_page.js
@@ -1,15 +1,14 @@
 Template.businessPage.helpers({
-isOwner: function() {
-    return this.userId == Meteor.userId();
-  },
-  menu: function() {
-    return Menu.find({businessId: this._id});
-  }
+	isOwner: function() {
+    	return this.userId == Meteor.userId();
+  	},
+  	menu: function() {
+    	return Menu.find({businessId: this._id});
+  	}
 });
 Template.businessPage.events({
 	'click #add': function(e) {
     e.preventDefault();
     Router.go("itemSubmit",{_id: Router.current().params._id});
   }
-
 });
