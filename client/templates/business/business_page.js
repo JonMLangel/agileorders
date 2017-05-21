ce9ef9cf77b1bc9ce692dc59d1d64f2f30e8ecf2
John Popilek <john.popilek@gmail.com>
2017-05-21 14:53:36 -0500

    added sorting logic

--- client/templates/business/business_page.js
+++ client/templates/business/business_page.js
@@ -3,8 +3,27 @@ Template.businessPage.helpers({
     	return this.userId == Meteor.userId();
   	},
   	menu: function() {
+      //keeping this it may be useful later
     	return Menu.find({businessId: this._id});
-  	}
+  	},
+    appetizers: function(){
+      return Menu.find({businessId: this._id, itemType: "Appetizer"});
+    },
+    meals: function(){
+      return Menu.find({businessId: this._id, itemType: "Meal"});
+    },
+    entrees: function(){
+      return Menu.find({businessId: this._id, itemType: "Entree"});
+    },
+    sides: function(){
+      return Menu.find({businessId: this._id, itemType: "Side"});
+    },
+    drinks: function(){
+      return Menu.find({businessId: this._id, itemType: "Drink"});
+    },
+    desserts: function(){
+      return Menu.find({businessId: this._id, itemType: "Dessert"});
+    }
 });
 Template.businessPage.events({
 	'click #add': function(e) {
